2d1cd0b7dc9b7f3991eaf0513368b958a1164f09
Jonny Bergdahl <jonny@bergdahl.local>
2017-01-27 03:09:37 +0100

    Finalized library files

--- src/M2M_LM75A.cpp
+++ src/M2M_LM75A.cpp
@@ -1,14 +1,11 @@
 //---------------------------------------------------------------------------------------------
 //
-// Library for the LM75A sensor by NXP, TI, et al.
-// Copyright 2016, M2M Solutions AB
+// Library for the LM75A temperature sensor by NXP and Texas Instruments.
 //
-// Jonny Bergdahl, 2016-11-18
+// Copyright 2016-2017, M2M Solutions AB
+// Written by Jonny Bergdahl, 2016-11-18
 //
-//---------------------------------------------------------------------------------------------
-// TODO: 
-//
-//---------------------------------------------------------------------------------------------
+// Licensed under the MIT license, see the LICENSE.txt file.
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////
 #include <Wire.h>
@@ -18,106 +15,118 @@
 //
 // Constructor
 //
-LM75A::LM75A()
+M2M_LM75A::M2M_LM75A()
 {
 	_i2cAddress = LM75A_DEFAULT_ADDRESS;
 }
 
-LM75A::LM75A (uint8_t i2cAddress)
+M2M_LM75A::M2M_LM75A(uint8_t i2cAddress)
 {
 	_i2cAddress = i2cAddress;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 //
-// Public functions
+//// Power management
 //
-void LM75A::shutdown()
+void M2M_LM75A::shutdown()
 {
 	uint8_t config = read8bitRegister(LM75A_REGISTER_CONFIG);
 	write8bitRegister(LM75A_REGISTER_CONFIG, (config & 0b11111110) | 0b1);
 }
 
-void LM75A::wakeup()
+void M2M_LM75A::wakeup()
 {
 	uint8_t config = read8bitRegister(LM75A_REGISTER_CONFIG);
 	write8bitRegister(LM75A_REGISTER_CONFIG, config & 0b11111110);
 }
 
-bool LM75A::isShutdown()
+bool M2M_LM75A::isShutdown()
 {
 	return (read8bitRegister(LM75A_REGISTER_CONFIG) & 0b1) == 1;	
 }
 
-float LM75A::getTemperature()
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Temperature functions
+//
+float M2M_LM75A::getTemperature()
 {
-	return (int16_t)read16bitRegister(LM75A_REGISTER_TEMP) / 256.0f;
+	uint16_t result;
+	if (!read16bitRegister(LM75A_REGISTER_TEMP, result))
+	{
+		return LM75A_INVALID_TEMPERATURE;
+	}
+	return (float)result / 256.0f;
 }
 
-float LM75A::getTemperatureInFarenheit()
+float M2M_LM75A::getTemperatureInFarenheit()
 {
 	return getTemperature() * 1.8f + 32.0f;
 }
 
-float LM75A::getHysterisisTemperature()
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Configuration functions
+//
+float M2M_LM75A::getHysterisisTemperature()
 {
-	return (float)read16bitRegister(LM75A_REGISTER_THYST) / 256.0f;
+	uint16_t result;
+	if (!read16bitRegister(LM75A_REGISTER_THYST, result))
+	{
+		return LM75A_INVALID_TEMPERATURE;
+	}
+	return (float)result / 256.0f;
 }
 
-FaultQueueValue LM75A::getFaultQueueValue()
+FaultQueueValue M2M_LM75A::getFaultQueueValue()
 {
 	return (FaultQueueValue)(read8bitRegister(LM75A_REGISTER_CONFIG) & 0b00011000);
 }
 
-float LM75A::getOSTripTemperature()
+float M2M_LM75A::getOSTripTemperature()
 {
-	return (float)read16bitRegister(LM75A_REGISTER_TOS) / 256.0f;
+	uint16_t result;
+	if (!read16bitRegister(LM75A_REGISTER_TOS, result))
+	{
+		return LM75A_INVALID_TEMPERATURE;
+	}
+	return (float)result / 256.0f;
 }
 
-OsPolarity LM75A::getOsPolarity()
+OsPolarity M2M_LM75A::getOsPolarity()
 {
 	return (OsPolarity)(read8bitRegister(LM75A_REGISTER_CONFIG) & 0b100);
 }
 
-DeviceMode LM75A::getDeviceMode()
+DeviceMode M2M_LM75A::getDeviceMode()
 {
 	return (DeviceMode)(read8bitRegister(LM75A_REGISTER_CONFIG) & 0b010);
 }
 
-uint8_t LM75A::getConfig()
-{
-	return read8bitRegister(LM75A_REGISTER_CONFIG);
-}
-
-float LM75A::getProdId()
-{
-	uint8_t value = read8bitRegister(LM75A_REGISTER_PRODID);
-	return (float)(value >> 4) + (value & 0x0F) / 10.0f;
-} 
-
-void LM75A::setHysterisisTemperature(float temperature)
+void M2M_LM75A::setHysterisisTemperature(float temperature)
 {
 	write16bitRegister(LM75A_REGISTER_THYST, temperature * 256);
 }
 
-void LM75A::setOsTripTemperature(float temperature)
+void M2M_LM75A::setOsTripTemperature(float temperature)
 {
 	write16bitRegister(LM75A_REGISTER_TOS, temperature * 256);
 }
 
-void LM75A::setFaultQueueValue(FaultQueueValue value)
+void M2M_LM75A::setFaultQueueValue(FaultQueueValue value)
 {
 	uint8_t config = read8bitRegister(LM75A_REGISTER_CONFIG);
 	write8bitRegister(LM75A_REGISTER_CONFIG, (config & 0b11100111) | value);
 }
 
-void LM75A::setOsPolarity(OsPolarity polarity)
+void M2M_LM75A::setOsPolarity(OsPolarity polarity)
 {
 	uint8_t config = read8bitRegister(LM75A_REGISTER_CONFIG);
 	write8bitRegister(LM75A_REGISTER_CONFIG, (config & 0b11111011) | polarity);
 }
 
-void LM75A::setDeviceMode(DeviceMode deviceMode)
+void M2M_LM75A::setDeviceMode(DeviceMode deviceMode)
 {
 	uint8_t config = read8bitRegister(LM75A_REGISTER_CONFIG);
 	write8bitRegister(LM75A_REGISTER_CONFIG, (config & 0b11111101) | deviceMode);
@@ -125,57 +134,74 @@ void LM75A::setDeviceMode(DeviceMode deviceMode)
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 //
+// Other
+//
+
+bool M2M_LM75A::isConnected()
+{
+	uint8_t oldValue = read8bitRegister(LM75A_REGISTER_CONFIG);
+	write8bitRegister(LM75A_REGISTER_CONFIG, 0x0f);
+	uint8_t newValue = read8bitRegister(LM75A_REGISTER_CONFIG);
+	write8bitRegister(LM75A_REGISTER_CONFIG, oldValue);
+	return newValue == 0x0f;
+}
+
+uint8_t M2M_LM75A::getConfig()
+{
+	return read8bitRegister(LM75A_REGISTER_CONFIG);
+}
+
+float M2M_LM75A::getProdId()
+{
+	uint8_t value = read8bitRegister(LM75A_REGISTER_PRODID);
+	return (float)(value >> 4) + (value & 0x0F) / 10.0f;
+} 
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
 // Private functions
 //
 
-uint8_t LM75A::read8bitRegister(const uint8_t reg)
+uint8_t M2M_LM75A::read8bitRegister(const uint8_t reg)
 {
 	uint8_t result;
-	int8_t response;
 
 	Wire.beginTransmission(_i2cAddress);
 	Wire.write(reg);
-	result = Wire.endTransmission();
-	if (result != 0)
-	{
-		return result;
-	}
-
+	Wire.endTransmission();
 	Wire.requestFrom(_i2cAddress, (uint8_t)1);
-	if (Wire.available() != 1)
-	{
-		return 0;
-	}
-
-	response = Wire.read();
-
-	return response;
+	result = Wire.read();
+	return result;
 }
 
-uint16_t LM75A::read16bitRegister(const uint8_t reg)
+bool M2M_LM75A::read16bitRegister(const uint8_t reg, uint16_t& response)
 {
 	uint8_t result;
-	int16_t response;
+
 	Wire.beginTransmission(_i2cAddress);
 	Wire.write(reg);
 	result = Wire.endTransmission();
+	// result is 0-4 
 	if (result != 0)
 	{
-		return result;
+		return false;
 	}
 
-	Wire.requestFrom(_i2cAddress, (uint8_t)2);
-	if (Wire.available() != 2)
+	result = Wire.requestFrom(_i2cAddress, (uint8_t)2);
+	if (result != 2)
 	{
-		Serial.println("ERROR READING");
-		
-		return 0;
+		return false;
 	}
-	response = (Wire.read() << 8) | Wire.read();
-	return response;
+	uint8_t part1 = Wire.read();
+	uint8_t part2 = Wire.read();
+	
+	//response = (Wire.read() << 8) | Wire.read();
+	uint16_t temp = part1 << 8 | part2;
+	response = part1 << 8 | part2;
+	return true;
 }
 
-bool LM75A::write16bitRegister(const uint8_t reg, const uint16_t value)
+bool M2M_LM75A::write16bitRegister(const uint8_t reg, const uint16_t value)
 {
 	Wire.beginTransmission(_i2cAddress);
 	Wire.write(reg);
@@ -184,7 +210,7 @@ bool LM75A::write16bitRegister(const uint8_t reg, const uint16_t value)
 	return Wire.endTransmission() == 0;
 }
 
-bool LM75A::write8bitRegister(const uint8_t reg, const uint8_t value)
+bool M2M_LM75A::write8bitRegister(const uint8_t reg, const uint8_t value)
 {
 	Wire.beginTransmission(_i2cAddress);
 	Wire.write(reg);
--- src/M2M_LM75A.h
+++ src/M2M_LM75A.h
@@ -2,29 +2,10 @@
 //
 // Library for the LM75A temperature sensor by NXP and Texas Instruments.
 //
-// Copyright 2016, M2M Solutions AB
-// Jonny Bergdahl, 2016-11-18
+// Copyright 2016-2017, M2M Solutions AB
+// Written by Jonny Bergdahl, 2016-11-18
 //
-//---------------------------------------------------------------------------------------------
-// LM75A is a temperature sensor communicating over the I2C protocol.
-//
-// The NXP version of LM75A has 11 significant bits of the temperature, which gives a 
-// resolution of 0.125 °C. The standard version only uses 9 bits, which gives a resuolution 
-// of 0.5 °C.
-//
-// I2C address span is 0x48 - 0x4F, and is set with pins A0/A1/A2.
-//
-// Power up mode:
-// Comparator mode
-// Tos = 80 °C
-// Thyst = 75 °C
-// OS active low
-//
-// Pin 3 is the OS (overtemperature shutdown). It can be used to to trigger a pin interrupt.
-// Check the functional mode, Tos and Thyst if you use this.
-//
-// http://www.nxp.com/documents/data_sheet/LM75A.pdf
-// http://www.ti.com/lit/ds/symlink/lm75a.pdf
+// Licensed under the MIT license, see the LICENSE.txt file.
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -36,17 +17,18 @@
 #include "WProgram.h"
 #endif
 
-#define LM75A_DEFAULT_ADDRESS		0x48	// Address is configured with pins A0-A2
-#define LM75A_INVALID_TEMPERATURE	-1000	// Just an arbritary value outside of the sensor limits
-#define LM75A_REGISTER_TEMP			0		// Temperature register (read-only)
-#define LM75A_REGISTER_CONFIG		1		// Configuration register
-#define LM75A_REGISTER_THYST		2		// Hysterisis register
-#define LM75A_REGISTER_TOS			3		// OS register
-#define LM75A_REGISTER_PRODID		7		// Product ID register - Only valid for Texas Instruments
+#define LM75A_DEFAULT_ADDRESS		0x48		// Address is configured with pins A0-A2
+#define LM75A_REGISTER_TEMP			0			// Temperature register (read-only)
+#define LM75A_REGISTER_CONFIG		1			// Configuration register
+#define LM75A_REGISTER_THYST		2			// Hysterisis register
+#define LM75A_REGISTER_TOS			3			// OS register
+#define LM75A_REGISTER_PRODID		7			// Product ID register - Only valid for Texas Instruments
 
-#define LM75_CONF_OS_COMP_INT		1
-#define LM75_CONF_OS_POL			2
-#define LM75_CONF_OS_F_QUE			3
+#define LM75_CONF_OS_COMP_INT		1			// OS operation mode selection
+#define LM75_CONF_OS_POL			2			// OS polarity selection
+#define LM75_CONF_OS_F_QUE			3			// OS fault queue programming
+
+#define LM75A_INVALID_TEMPERATURE	-1000.0f	// Just an arbritary value outside of the sensor limits
 
 enum FaultQueueValue : uint8_t
 {
@@ -68,21 +50,21 @@ enum DeviceMode : uint8_t
 	DEVICE_MODE_INTERRUPT = 0b10
 };
 
-class LM75A 
+class M2M_LM75A 
 {
 	// Private variables
 	uint8_t _i2cAddress;
 
 	// Private functions
-	uint8_t read8bitRegister(uint8_t register);
-	uint16_t read16bitRegister(uint8_t register);
+	uint8_t read8bitRegister(const uint8_t reg);
+	bool read16bitRegister(uint8_t register, uint16_t& response);
 	bool write16bitRegister(const uint8_t reg, const uint16_t value);
 	bool write8bitRegister(const uint8_t reg, const uint8_t value);
 
 public:
 	// Constructors
-    LM75A();
-    LM75A(byte address);
+    M2M_LM75A();
+    M2M_LM75A(uint8_t address);
 
 	// Power management
 	void shutdown();
@@ -93,23 +75,22 @@ public:
     float getTemperature();
 	float getTemperatureInFarenheit();
 
-	// Read config
+	// Configuration functions
 	float getHysterisisTemperature();	
 	FaultQueueValue getFaultQueueValue();
 	float getOSTripTemperature();
 	OsPolarity getOsPolarity();
 	DeviceMode getDeviceMode();
+	void setHysterisisTemperature(const float temperature);
+	void setOsTripTemperature(const float temperature);
+	void setFaultQueueValue(const FaultQueueValue value);
+	void setOsPolarity(const OsPolarity polarity);
+	void setDeviceMode(const DeviceMode deviceMode);	
 
 	// Other
+	bool isConnected();
 	uint8_t getConfig();
 	float getProdId();
-
-	// Write config
-	void setHysterisisTemperature(float temperature);
-	void setOsTripTemperature(float temperature);
-	void setFaultQueueValue(FaultQueueValue value);
-	void setOsPolarity(OsPolarity polarity);
-	void setDeviceMode(DeviceMode deviceMode);	
 };
 
 #endif
